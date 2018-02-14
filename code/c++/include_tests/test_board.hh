e2c5a7e48fadf70e6e01bb3350f2cef576903f2f
Probak <probak@DebienIlBugMoins.Ma>
2018-02-14 12:55:30 +0100

    Ajout de la fonction permettant de tester l'égalité de deux bitboard

--- code/c++/include_tests/test_board.hh
+++ code/c++/include_tests/test_board.hh
@@ -10,14 +10,31 @@ namespace tests
   class test_board : public CppUnit::TestCase
   {
     CPPUNIT_TEST_SUITE (test_board);
+    CPPUNIT_TEST (dummy_test);
     CPPUNIT_TEST_SUITE_END ();
     
   private:
     reversi::board* _fixture;
-        
+	
+    /*
+     * Function used to check if a bitboard correspond the wanted
+     * value by dividing it in two __int64.
+     */
+    void check_bitboard (__int128 expected, __int128 acutal);
   public:
+
+    /*
+     * Used for the instanciation of _fixture.
+     */
     void setUp ();
+
+    /*
+     * Used for freeing _fixture.
+     */
     void tearDown ();
+
+    /* TO DELETE */
+    void dummy_test ();
   };
 }
 
--- code/c++/tests/test_board.cc
+++ code/c++/tests/test_board.cc
@@ -1,6 +1,8 @@
 #include "test_board.hh"
 #include <iostream>
 
+#define HALF_BITBOARD 64
+
 namespace tests
 {
   CPPUNIT_TEST_SUITE_REGISTRATION (test_board);
@@ -14,5 +16,26 @@ namespace tests
   {
     delete _fixture;
     _fixture = NULL;
+  }
+
+  void test_board::check_bitboard (__int128 expected, __int128 actual)
+  {
+    /* Splitting the first int128. */
+    uint64_t expected_first_half = expected;
+    uint64_t expected_second_half = expected>>HALF_BITBOARD;
+    /* Splitting the second int128 */
+    uint64_t actual_first_half = actual;
+    uint64_t actual_second_half = actual>>HALF_BITBOARD;
+    
+    CPPUNIT_ASSERT_EQUAL (expected_first_half, actual_first_half);
+    CPPUNIT_ASSERT_EQUAL (expected_second_half, actual_second_half);
+    
+  }
+
+  void test_board::dummy_test ()
+  {
+    reversi::board::bitboard bit1 = 127;
+    reversi::board::bitboard bit2 = 127;
+    check_bitboard (bit1, bit2);
   }
 }
