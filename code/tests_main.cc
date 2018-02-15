f21b805b52b57d79b19cfb6a596302b6e0b5211d
Julien Pilleux <julien.pilleux@etu.u-bordeaux.fr>
2018-02-15 10:21:35 +0100

    Modification de l'organisation des fichiers

---
+++ code/tests_main.cc
@@ -0,0 +1,12 @@
+#include "test_board.hh"
+#include <cppunit/ui/text/TestRunner.h>
+
+int main (int argc, char *argv[])
+{
+  CppUnit::TextUi::TestRunner runner;
+  
+  runner.addTest(CppUnit::TestFactoryRegistry::getRegistry().makeTest());
+  runner.run();
+  
+  return EXIT_SUCCESS;
+}
